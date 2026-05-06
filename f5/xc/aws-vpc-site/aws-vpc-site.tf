ade4efe0e155d46a1ced6bd4a2ca3c640e7c8109
drpotters <daverpotter2@gmail.com>
2026-05-05 19:15:16 -0700

    change module source and version for testing

--- f5/xc/aws-vpc-site/aws-vpc-site.tf
+++ f5/xc/aws-vpc-site/aws-vpc-site.tf
@@ -1,5 +1,5 @@
-module "aws_vpc_site" {
-  source                                   = "f5devcentral/aws-vpc-site-test/xc"
+module "aws_vpc_site_test" {
+  source                                   = "app.terraform.io/F5-DevCentral/aws-vpc-site-test/xc"
   version                                  = "1.0.0"
   site_name                                = ("" != var.prefix) ? format("%s-%s", var.prefix, var.name) : var.name
   aws_region                               = var.aws_region

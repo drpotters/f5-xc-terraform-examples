d3262ca416bafb6c59ede80c29e891e52cac9ef3
drpotters <daverpotter2@gmail.com>
2026-07-09 16:10:07 -0700

    Undo 861c03a changes for AWS/Azure site workflows and legacy site folders

--- f5/xc/azure-vnet-site/main.tf
+++ f5/xc/azure-vnet-site/main.tf
@@ -1,129 +1,20 @@
-locals {
-  name_prefix = "${var.node_prefix}-${random_string.rand_name.result}"
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
 }
 
-resource "random_string" "rand_name" {
-  length  = 4
-  lower   = true
-  upper   = false
-  special = false
-  numeric = false
+provider "azuread" {
+  client_id     = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id     = var.azure_tenant_id
 }
 
-resource "tls_private_key" "adminuserkey" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-
-data "azurerm_resource_group" "main" {
-  name = var.az_rg_name
-}
-
-data "azurerm_virtual_network" "sm2_vnet" {
-  name                = var.az_virtual_network
-  resource_group_name = var.az_rg_name
-}
-
-data "azurerm_subnet" "public" {
-  name                 = var.az_public_subnet_name
-  virtual_network_name = data.azurerm_virtual_network.sm2_vnet.name
-  resource_group_name  = var.az_rg_name
-}
-
-data "azurerm_subnet" "private" {
-  name                 = var.az_private_subnet_name
-  virtual_network_name = data.azurerm_virtual_network.sm2_vnet.name
-  resource_group_name  = var.az_rg_name
-}
-
-data "azurerm_network_security_group" "sm2_nsg" {
-  count               = var.az_nsg_name != "" ? 1 : 0
-  name                = var.az_nsg_name
-  resource_group_name = var.az_rg_name
-}
-
-resource "azurerm_public_ip" "smv2_slo_public_ip" {
-  count               = var.instances
-  name                = "${local.name_prefix}-pub-ip-${random_string.rand_name.result}-${count.index + 1}"
-  location            = var.az_location
-  resource_group_name = var.az_rg_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "slo_nic" {
-  count               = var.instances
-  name                = "${local.name_prefix}-slo-nic-${random_string.rand_name.result}-${count.index + 1}"
-  location            = var.az_location
-  resource_group_name = var.az_rg_name
-  accelerated_networking_enabled = true
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.public.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.smv2_slo_public_ip[count.index].id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "slo_nic_nsg_assoc" {
-  count                     = var.az_nsg_name != "" ? var.instances : 0
-  network_interface_id      = azurerm_network_interface.slo_nic[count.index].id
-  network_security_group_id = data.azurerm_network_security_group.sm2_nsg[0].id
-}
-
-resource "azurerm_network_interface" "sli_nic" {
-  count               = var.instances
-  name                = "${local.name_prefix}-sli-nic-${random_string.rand_name.result}-${count.index + 1}"
-  location            = var.az_location
-  resource_group_name = var.az_rg_name
-  accelerated_networking_enabled = true
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.private.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "smv2_instance" {
-  count               = var.instances
-  name                = "${local.name_prefix}-${count.index + 1}"
-  resource_group_name = var.az_rg_name
-  location            = var.az_location
-  size                = var.sku_flavor
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.slo_nic[count.index].id,
-    azurerm_network_interface.sli_nic[count.index].id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = var.disk_size
-  }
-
-  source_image_reference {
-    publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
-  }
-
-  plan {
-    name      = var.image_sku
-    product   = var.image_offer
-    publisher = var.image_publisher
-  }
-
-  custom_data = base64encode(templatefile("${path.module}/templates/cloud-config-base.tmpl", {
-    cluster_token = var.cluster_token
-  }))
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = tls_private_key.adminuserkey.public_key_openssh
-  }
+provider "volterra" {
+  api_p12_file = var.xc_api_p12_file
+  url          = var.xc_api_url
 }
--- f5/xc/azure-vnet-site/outputs.tf
+++ f5/xc/azure-vnet-site/outputs.tf
@@ -1,23 +1,102 @@
-output "sm_azure_vm_ips" {
-  value = azurerm_public_ip.smv2_slo_public_ip[*].ip_address
+output "name" {
+  description = "Name of the configured Azure VNET Site."
+  value       = module.azure_vnet_site.name
 }
 
-output "slo_nic_names" {
-  value = azurerm_network_interface.slo_nic[*].name
+output "id" {
+  description = "ID of the configured Azure VNET Site."
+  value       = module.azure_vnet_site.id
 }
 
-output "slo_nic_private_ips" {
-  value = azurerm_network_interface.slo_nic[*].private_ip_address
+output "ssh_private_key_pem" {
+  description = "Azure VNET Site generated private key."
+  value       = module.azure_vnet_site.ssh_private_key_pem
+  sensitive   = true
+}
+
+output "ssh_private_key_openssh" {
+  description = "Azure VNET Site generated OpenSSH private key."
+  value       = module.azure_vnet_site.ssh_private_key_openssh
+  sensitive   = true
+}
+
+output "ssh_public_key" {
+  description = "Azure VNET Site public key."
+  value       = module.azure_vnet_site.ssh_public_key
+}
+
+# output "apply_tf_output" {
+#   description = "Azure VNET Site apply terraform output parameter."
+#   value       = module.azure_vnet_site.apply_tf_output
+# }
+
+output "apply_tf_output_map" {
+  description = "Azure VNET Site apply terraform output parameter."
+  value       = module.azure_vnet_site.apply_tf_output_map
+}
+
+output "master_nodes_az_names" {
+  description = "Azure VNET Site master nodes availability zone names."
+  value       = module.azure_vnet_site.master_nodes_az_names
+}
+
+output "vnet_resource_group" {
+  description = "Azure VNET resource group name."
+  value       = module.azure_vnet_site.vnet_resource_group
+}
+
+output "vnet_name" {
+  description = "Azure VNET name."
+  value       = module.azure_vnet_site.vnet_name
+}
+
+
+output "inside_rt_names" {
+  description = "Azure VNET inside route table name."
+  value       =  module.azure_vnet_site.inside_rt_names
+}
+
+output "location" {
+  description = "Azure Resources Location."
+  value       = module.azure_vnet_site.location
+}
+
+output "site_resource_group" {
+  description = "Azure VNET Site resource group name."
+  value       = module.azure_vnet_site.site_resource_group
+}
+
+output "sli_nic_ids" {
+  description = "Azure VNET Site SLI NIC IDs."
+  value       = module.azure_vnet_site.sli_nic_ids
 }
 
 output "sli_nic_names" {
-  value = azurerm_network_interface.sli_nic[*].name
+  description = "Azure VNET Site SLI NIC names."
+  value       = module.azure_vnet_site.sli_nic_names
 }
 
 output "sli_nic_private_ips" {
-  value = azurerm_network_interface.sli_nic[*].private_ip_address
+  description = "Azure VNET Site SLI NIC private IPs."
+  value       = module.azure_vnet_site.sli_nic_private_ips
 }
 
-output "vm_names" {
-  value = azurerm_linux_virtual_machine.smv2_instance[*].name
+output "slo_nic_ids" {
+  description = "Azure VNET Site SLO NIC IDs."
+  value       = module.azure_vnet_site.slo_nic_ids
+}
+
+output "slo_nic_names" {
+  description = "Azure VNET Site SLO NIC names."
+  value       = module.azure_vnet_site.slo_nic_names
+}
+
+output "slo_nic_private_ips" {
+  description = "Azure VNET Site SLO NIC private IPs."
+  value       = module.azure_vnet_site.slo_nic_private_ips
+}
+
+output "slo_nic_public_ips" {
+  description = "Azure VNET Site SLO NIC public IPs."
+  value       = module.azure_vnet_site.slo_nic_public_ips
 }
